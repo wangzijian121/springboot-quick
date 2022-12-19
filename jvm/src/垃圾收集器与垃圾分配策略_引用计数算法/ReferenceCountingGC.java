f2a01290c780614234ce94f32e85580334590c0f
zijian Wang <1219116968@qq.com>
2022-12-19 14:36:51 +0800

    feature： 添加部分类

---
+++ jvm/src/垃圾收集器与垃圾分配策略_引用计数算法/ReferenceCountingGC.java
@@ -0,0 +1,26 @@
+package 垃圾收集器与垃圾分配策略_引用计数算法;
+
+public class ReferenceCountingGC {
+    public Object instance = null;
+    private static final int _1MB = 1024 * 1024;
+    /**
+     * 这个成员属性的唯一意义就是占点内存，以便能在 GC 日志中看清楚是否有回收过
+     */
+    private byte[] bigSize = new byte[2 * _1MB];
+
+    public static void testGC() {
+        ReferenceCountingGC objA = new ReferenceCountingGC();
+        ReferenceCountingGC objB = new ReferenceCountingGC();
+        objA.instance = objB;
+        objB.instance = objA;
+        objA = null;
+        objB = null;
+        // 假设在这行发生 GC，objA 和 objB 是否能被回收？
+        System.gc();
+    }
+
+    public static void main(String[] args) {
+        testGC();
+    }
+
+}
