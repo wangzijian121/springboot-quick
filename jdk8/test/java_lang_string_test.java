d68b69f4aaafac808387d561967b375ab95d315b
wangzijian <1219116968@qq.com>
2023-05-10 18:28:48 +0800

    【学习】【验证】: java.lang.String

---
+++ jdk8/test/java_lang_string_test.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+public class java_lang_string_test {
+
+
+    @Test
+    public void value_test(){
+
+        String name ="wangzijian";
+        String name2 ="wangzijian";
+        System.out.println(name.hashCode());
+        System.out.println(name2.hashCode());
+    }
+
+
+    @Test
+    public void sub_test(){
+
+        String name ="wangzijian";
+        System.out.println(name.substring(1));//angzijian
+        System.out.println(name.substring(2,4));//ng
+        System.out.println(name.subSequence(2,4));//ng
+    }
+
+}
