9fd0f37e4cc3d5781e63bd7b4cf06f33666a4acc
wangzijian <1219116968@qq.com>
2023-05-13 14:42:58 +0800

    【学习】【验证】: ArrayStoreException.java

---
+++ jdk8/test/com/example/lang/ArrayStoreExceptionTest.java
@@ -0,0 +1,11 @@
+package com.example.lang;
+
+import org.junit.Test;
+
+public class ArrayStoreExceptionTest {
+    @Test
+    public void dummy() {
+        Object[] x = new String[3];
+        x[0] = 0;//java.lang.ArrayStoreException: java.lang.Integer
+    }
+}
