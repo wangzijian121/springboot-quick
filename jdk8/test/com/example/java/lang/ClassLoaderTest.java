3017948780c74b7fe994d674ff4abf53eea3bb3d
wangzijian <1219116968@qq.com>
2023-05-16 17:58:42 +0800

    【学习】【验证】: ClassLoader +更新代码清单

---
+++ jdk8/test/com/example/java/lang/ClassLoaderTest.java
@@ -0,0 +1,170 @@
+package com.example.java.lang;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClassLoaderTest {
+
+    @Test
+    public void testArr() {
+        String[] str = new String[]{"wangzijian"};
+        System.out.println(str.getClass().getClassLoader());//null
+    }
+
+    @Test
+    public void addClass() {
+    }
+
+    @Test
+    public void loadClass() {
+    }
+
+    @Test
+    public void testLoadClass() {
+    }
+
+    @Test
+    public void getClassLoadingLock() {
+    }
+
+    @Test
+    public void findClass() {
+    }
+
+    @Test
+    public void defineClass() {
+    }
+
+    @Test
+    public void testDefineClass() {
+    }
+
+    @Test
+    public void testDefineClass1() {
+    }
+
+    @Test
+    public void testDefineClass2() {
+    }
+
+    @Test
+    public void resolveClass() {
+    }
+
+    @Test
+    public void findSystemClass() {
+    }
+
+    @Test
+    public void findLoadedClass() {
+    }
+
+    @Test
+    public void setSigners() {
+    }
+
+    @Test
+    public void getResource() {
+    }
+
+    @Test
+    public void getResources() {
+    }
+
+    @Test
+    public void findResource() {
+    }
+
+    @Test
+    public void findResources() {
+    }
+
+    @Test
+    public void registerAsParallelCapable() {
+    }
+
+    @Test
+    public void getSystemResource() {
+    }
+
+    @Test
+    public void getSystemResources() {
+    }
+
+    @Test
+    public void getBootstrapClassPath() {
+    }
+
+    @Test
+    public void getResourceAsStream() {
+    }
+
+    @Test
+    public void getSystemResourceAsStream() {
+    }
+
+    @Test
+    public void getParent() {
+    }
+
+    @Test
+    public void getSystemClassLoader() {
+    }
+
+    @Test
+    public void isAncestor() {
+    }
+
+    @Test
+    public void getClassLoader() {
+    }
+
+    @Test
+    public void checkClassLoaderPermission() {
+    }
+
+    @Test
+    public void definePackage() {
+    }
+
+    @Test
+    public void getPackage() {
+    }
+
+    @Test
+    public void getPackages() {
+    }
+
+    @Test
+    public void findLibrary() {
+    }
+
+    @Test
+    public void loadLibrary() {
+    }
+
+    @Test
+    public void findNative() {
+    }
+
+    @Test
+    public void setDefaultAssertionStatus() {
+    }
+
+    @Test
+    public void setPackageAssertionStatus() {
+    }
+
+    @Test
+    public void setClassAssertionStatus() {
+    }
+
+    @Test
+    public void clearAssertionStatus() {
+    }
+
+    @Test
+    public void desiredAssertionStatus() {
+    }
+}
