bbedc9c66cef1dbd3f84584e590f28eecdc61c85
wangzijian <1219116968@qq.com>
2023-05-23 11:27:42 +0800

    【学习】【验证】: 解决SimUDuck橡皮鸭会飞的bug #4

--- design-patterns/src/duck/RubberDuck.java
+++ design-patterns/src/duck/RubberDuck.java
@@ -8,14 +8,12 @@ public class RubberDuck extends Duck {
 
     @Override
     public void swim() {
-
         System.out.println("橡皮鸭游泳");
     }
 
     @Override
     public void fly() {
-
-        System.out.println("橡皮鸭在飞！??? are you kidding me???");
+        //no action
     }
 
     @Override
