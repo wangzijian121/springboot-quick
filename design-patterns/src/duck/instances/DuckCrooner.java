a58391278827c805cd7b1010435f96eff542e5e1
wangzijian <1219116968@qq.com>
2023-05-27 14:39:05 +0800

    【学习】【实践】【优化】: 鸭鸣器的实现 #9

---
+++ design-patterns/src/duck/instances/DuckCrooner.java
@@ -0,0 +1,22 @@
+package duck.instances;
+
+import duck.QuackBehavior;
+import duck.impl.Quack;
+
+/**
+ * 鸭鸣器，不属于鸭子。
+ */
+public class DuckCrooner {
+
+    //叫
+    QuackBehavior quackBehavior = new Quack();
+
+    public void quack() {
+        quackBehavior.quack();
+    }
+
+    public static void main(String[] args) {
+        DuckCrooner duckCrooner=new DuckCrooner();
+        duckCrooner.quack();
+    }
+}
