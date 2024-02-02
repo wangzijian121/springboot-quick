a0b4cce66c9efcd5b13c2e63e5dbb71bcb16e78c
zijian Wang <1219116968@qq.com>
2024-02-02 09:35:41 +0800

    feature : 数据类型

---
+++ java/src/main/java/数据类型/Demo.java
@@ -0,0 +1,36 @@
+package 数据类型;
+
+/**
+ * @author zijian Wang
+ */
+public class Demo {
+    public static void main(String[] args) {
+        //1字节
+        byte b = 127;
+        //2字节
+        short s = 32767;
+        //4字节
+        int i = 2147483647;
+        //8字节
+        long l = 9223372036854775807L;
+        System.out.println(b);
+        System.out.println(b);
+        System.out.println(i);
+        System.out.println(l);
+
+        //4字节
+        float f = 3.1415926535747f;
+        //8字节
+        double d = 3.1415926535747d;
+        System.out.println(f);
+        System.out.println(d);
+
+        //2字节
+        char c = 'A';
+        //1字节
+        boolean bo = false;
+        System.out.println(c);
+        System.out.println(bo);
+
+    }
+}
