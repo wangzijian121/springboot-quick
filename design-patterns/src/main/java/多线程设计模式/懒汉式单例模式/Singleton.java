ca6392f794cae57d97b76b694056cc04483ccd15
zijian Wang <1219116968@qq.com>
2024-04-17 16:50:34 +0800

    feature:懒汉汉式单例模式

---
+++ design-patterns/src/main/java/多线程设计模式/懒汉式单例模式/Singleton.java
@@ -0,0 +1,25 @@
+package 多线程设计模式.懒汉式单例模式;
+
+/***
+ * 懒汉式单例：解决了饿汉式的空间浪费的问题，但是会有多线程的问题。
+ * @author  zijian Wang
+ */
+public final class Singleton {
+
+    private static Singleton singleton = null;
+
+    public Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        //... 优化
+    }
+}
