52ca4acc27bdaf5cde95536fafd18679db3b7f18
zijian Wang <1219116968@qq.com>
2024-04-17 16:55:46 +0800

    feature:枚举实现单例

---
+++ design-patterns/src/main/java/多线程设计模式/枚举单例模式/SingletonEnum.java
@@ -0,0 +1,29 @@
+package 多线程设计模式.枚举单例模式;
+
+/**
+ * 枚举单例类
+ */
+public enum SingletonEnum {
+
+    INSTANCE;
+
+    // 可以在枚举中添加其他成员变量和方法
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+}
+
+class SingletonEnumTest {
+    public static void main(String[] args) {
+        SingletonEnum instance = SingletonEnum.INSTANCE;
+        instance.setValue(1);
+        System.out.println(instance.getValue());
+    }
+}
