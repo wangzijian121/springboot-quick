6aafd87212cd3e1b0145314c66425dbf92267e55
王子健 <1219116968@qq.com>
2024-05-22 17:12:31 +0800

    feature:Application 四个主要实现类的使用。

---
+++ springboot/src/main/java/com/zjyun/spring/applicationcontext/ApplicationContextTest.java
@@ -0,0 +1,56 @@
+package com.zjyun.spring.applicationcontext;
+
+import com.zjyun.spring.config.AppConfig;
+import com.zjyun.spring.config.WebConfig;
+import com.zjyun.spring.model.Bean1;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * @Description:
+ * @Author: Wang Zijian
+ * @Date: 2024/5/22
+ */
+public class ApplicationContextTest {
+    public static void main(String[] args) {
+
+        //通过resource 下的applicationContext.xml 解析获取Bean
+//        testClassPathXmlApplicationContext();
+        //通过系统路径下的XML解析获取bean
+//        testFileSystemXmlApplicationContext();
+        //通过注解解析获取
+//        testAnnotationConfigApplicationContext();
+        //通过注解配置解析 的web
+        testAnnotationConfigServletWebServerApplicationContext();
+    }
+
+    public static void testClassPathXmlApplicationContext() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("WEB-INF/applicationContext.xml");
+        System.out.println(context.getBean(Bean1.class));
+    }
+
+    public static void testFileSystemXmlApplicationContext() {
+        ApplicationContext context = new FileSystemXmlApplicationContext("D:\\项目\\王子健-Java\\Java\\springboot\\target\\classes\\WEB-INF\\applicationContext.xml");
+        System.out.println(context.getBean(Bean1.class));
+    }
+
+    public static void testAnnotationConfigApplicationContext() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        System.out.println(context.getBean("bean1"));
+        System.out.println(context.getBean("bean2"));
+
+    }
+
+    public static void testAnnotationConfigServletWebServerApplicationContext() {
+        //基于ServletWeb 服务器的容器实现
+        AnnotationConfigServletWebServerApplicationContext context =
+                new AnnotationConfigServletWebServerApplicationContext(WebConfig.class);
+
+//        context.getBean("bean1");
+//        context.getBean("bean2");
+    }
+}
---
+++ springboot/src/main/java/com/zjyun/spring/config/WebConfig.java
@@ -0,0 +1,50 @@
+package com.zjyun.spring.config;
+
+import com.zjyun.spring.model.Bean1;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Description:
+ * @Author: Wang Zijian
+ * @Date: 2024/5/22
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public DispatcherServletRegistrationBean registrationBean(DispatcherServlet dispatcherServlet) {
+        return new DispatcherServletRegistrationBean(dispatcherServlet, "/");
+    }
+    @Bean("/hello")
+    public Controller controller1() {
+        return new Controller() {
+            @Override
+            public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+                response.getWriter().write("hello world!!");
+                return null;
+            }
+        };
+    }
+}
