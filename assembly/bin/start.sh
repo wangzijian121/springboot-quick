abb7f0393d024db36637e005e25cdab5458847ef
王子健 <1219116968@qq.com>
2024-07-31 19:21:07 +0800

    update:update start and stop script at bin

--- assembly/bin/start.sh
+++ assembly/bin/start.sh
@@ -1,1 +1,30 @@
-echo "start"
+#!/bin/bash
+
+BIN_DIR=$(dirname $0)
+PROJECT_HOME=${PROJECT_HOME:-$(
+  cd $BIN_DIR/..
+  pwd
+)}
+
+JAVA_OPTS=${JAVA_OPTS:-"-server -Duser.timezone=${SPRING_JACKSON_TIME_ZONE}
+   -Xms1g
+   -Xmx1g
+   -Xmn512m
+   -XX:+UseG1GC
+   -XX:+PrintGCDetails
+   -Xloggc:gc.log
+   -XX:+HeapDumpOnOutOfMemoryError
+   -XX:HeapDumpPath=dump.hprof"}
+echo "$JAVA_HOME/bin/java $JAVA_OPTS \
+        -cp "$PROJECT_HOME/conf":"$PROJECT_HOME/libs/*" \
+        -Dspring.profiles.active=prod com.zjyun.springboot.Application"
+        
+nohup $JAVA_HOME/bin/java $JAVA_OPTS \
+  -cp "$PROJECT_HOME/conf":"$PROJECT_HOME/libs/*" \
+  -Dspring.profiles.active=prod  com.zjyun.springboot.Application >/dev/null 2>&1 &
+
+if [ $? -eq 0 ]; then
+  echo "Application started successfully."
+else
+  echo "Application failed to start."
+fi
--- assembly/bin/stop.sh
+++ assembly/bin/stop.sh
@@ -1,1 +1,13 @@
-echo "stop"
+#!/bin/bash
+
+PID=$(ps -ef | grep 'com.zjyun.springboot.Application' | grep -v grep | awk '{print $2}')
+
+if [ -z "$PID" ]; then
+    echo "No Application process found to stop"
+    exit 1
+fi
+
+echo "Stopping Application process $PID"
+kill $PID
+
+exit 0
