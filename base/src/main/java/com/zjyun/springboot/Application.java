2f9ad6045a68090efc3059768d14a3e7081335e9
王子健 <1219116968@qq.com>
2024-07-31 21:44:45 +0800

    feature:添加依赖和插件的依赖引用管理

--- base/src/main/java/com/zjyun/springboot/Application.java
+++ base/src/main/java/com/zjyun/springboot/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Wang Zijian
+ */
 @SpringBootApplication
 @RestController
 @MapperScan("com.zjyun.springboot.mapper")
